7a5869a724e7c2fa1a6ae23ece42769221a9c179
Lucas Avila da Fonseca <lucasafonss@gmail.com>
2026-04-01 00:18:28 -0300

    adiciona todas os exercícios sobre estrutura de repetição

--- Algorítimos/estruturaRepeticao.py
+++ Algorítimos/estruturaRepeticao.py
@@ -25,7 +25,8 @@
 ########## 
 #num1 = int(input('digite o primeiro numero: '))
 #num2 = int(input('digite o segundo '))
-#
+#if num1 == num2:
+#   print('os dois numeros nao podem ser iguais')
 #for i in range((num1 + 1), num2):
 #    print(i)
 ###########
@@ -38,14 +39,37 @@
 #        soma += i
 #print(f'a soma deu {soma} e tem {pares} numeros pares')
 ############
-palavra = str(input('Digite uma palavra: ').strip().lower())
-vogal = 0
-vogais = "aeiou"
+#palavra = str(input('Digite uma palavra: ').strip().lower())
+#vogal = 0
+#vogais = "aeiou"
+#
+#for letra in palavra:
+#    if letra in vogais:
+#        vogal += 1
+#
+#print(f"A palavra tem {vogal} vogais.")
+#############
+#reprovados = 0
+#for i in range(4):
+#    nota1 = float(input('digite a primeira nota: ').strip())
+#    nota2 = float(input('digite a segunda nota: ').strip())
+#    media = (nota1 + nota2) / 2
+#    if media < 5:
+#        reprovados += 1
+#print(f'tivemos {reprovados} alunos reprovados')
+#############
+#for i in range(1, 5):
+#    filho1 = int(input('digite a idade do primeiro filho: ').strip())
+#    filho2 = int(input('digite a idade do segundo filho: ').strip())
+#    soma = filho1 + filho2
+#    print(f'a soma da idade do casal de numero {i} deu {soma}')
+###
+casal = 0
+while casal != 4:
+    casal += 1
+    filho1 = int(input('digite a idade do primeiro filho: ').strip())
+    filho2 = int(input('digite a idade do segundo filho: ').strip())
+    soma = filho1 + filho2
+    print(f'a soma da idade do casal de numero {casal} deu {soma}')
 
-# "Para cada letra na palavra:"
-for letra in palavra:
-    if letra in vogais:
-        vogal += 1 # Forma mais curta de escrever vogal = vogal + 1
 
-print(f"A palavra tem {vogal} vogais.")
-print('bananas')
