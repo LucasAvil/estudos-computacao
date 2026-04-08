d1e171802a6188dac602131623833c6a92b4b4d9
Lucas Avila da Fonseca <lucasafonss@gmail.com>
2026-04-08 03:20:33 -0300

    faz com que o jokempo.py tenha suas funcionalidades originais, mantendo o md3 especificamente no jokempo_com_md3.py

--- Algorítimos/jokempo.py
+++ Algorítimos/jokempo.py
@@ -6,26 +6,23 @@ partidas = 0
 novamente = 'S'
 opcoes = ["Pedra", "Papel", "Tesoura"]
 aproveitamento = 1
-#while novamente == 'S':
-for i in range(0, 3):
+while novamente == 'S':
     jogador = str(input("escolha entre pedra papel ou tesoura: ").capitalize().strip())
 
     pc = random.choice(opcoes)
     if jogador in opcoes:
         if pc == jogador:
-            #print('Empate!')
-            continue
+            print('Empate!')
         elif pc == "Pedra" and jogador == "Papel" or pc == "Papel" and jogador == "Tesoura" or pc == "Tesoura" and jogador == "Pedra":
-            #print('Você ganhou!!')
+            print('Você ganhou!!')
             vitoria = vitoria + 1
         else:
-            #print('Você perdeu!')
-            continue
+            print('Você perdeu!')
 
         partidas = partidas + 1
-        #novamente = str(input(f'A máquina usou {pc} e você usou {jogador}, você deseja jogar novamente? s/n ').capitalize().strip())
         aproveitamento = (vitoria / partidas) * 100
-        #print(f'\nPontuação: {vitoria}\n Partidas: {partidas}\n Aproveitamento: {aproveitamento:.2f}% \n')
+        print(f'\nPontuação: {vitoria}\n Partidas: {partidas}\n Aproveitamento: {aproveitamento:.2f}% \n')
+        novamente = str(input(f'A máquina usou {pc} e você usou {jogador}, você deseja jogar novamente? s/n ').capitalize().strip())
 
     else:
         novamente = str(input('você escreveu alguma coisa errada, deseja repetir? s/n ').capitalize().strip())
@@ -36,12 +33,6 @@ for i in range(0, 3):
     while novamente not in ['S', 'N']:
         novamente = str(input('Opção inválida, gostaria de tentar novamente? s/n ').capitalize().strip())
         continue
-    #if partidas == 3:
-if vitoria >= 2:
-    novamente = str(input('parabéns, você venceu! quer jogar novamente? (s/n) ').capitalize().strip())
-    
-else:
-    novamente = str(input('jogo encerrado! a máquina venceu! quer jogar novamente? (s/n) ').capitalize().strip()) 
     
 print(f'Jogo encerrado! Obrigado por jogar!')
 
